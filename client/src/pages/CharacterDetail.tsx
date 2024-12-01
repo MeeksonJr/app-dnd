3ea4b1316d6bf02ecb5dd938ce72894700d3a48d
Mohamed Lamine Datt <161087278+MeeksonJr@users.noreply.github.com>
2024-11-30 22:04:19 -0500

    Update CharacterDetail.tsx

--- client/src/pages/CharacterDetail.tsx
+++ client/src/pages/CharacterDetail.tsx
@@ -2,9 +2,19 @@
 import { useParams } from 'react-router-dom';
 import '../styles/CharacterDetail.css';
 
-const CharacterDetail = ({ characters }) => {
-  const { id } = useParams();
-  const character = characters[id];
+
+interface Character {
+  name: string;
+  race: string;
+  classType: string;
+}
+
+interface CharacterDetailProps {
+  characters: Character[];
+}
+const CharacterDetail: React.FC<CharacterDetailProps> = ({ characters }) => {
+  const { id } = useParams<{ id: string }>();
+  const character = characters[parseInt(id ?? '', 10)];
 
   if (!character) {
     return <h2>Character not found</h2>;
